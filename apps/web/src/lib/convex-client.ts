98962139ce70a6ac50459708c47b60e160d82930
wahyu ikbal maulana <wahyuikbal777@gmail.com>
2026-05-07 09:58:14 +0700

    Update convex-client.ts

--- apps/web/src/lib/convex-client.ts
+++ apps/web/src/lib/convex-client.ts
@@ -3,7 +3,7 @@ import { ConvexReactClient } from "convex/react";
 const convexUrl = process.env.NEXT_PUBLIC_CONVEX_URL;
 
 if (!convexUrl) {
-	throw new Error("NEXT_PUBLIC_CONVEX_URL is not set");
+	throw new Error("NEXT_PUBLIC_CONVEX_URL is not set bjir");
 }
 
 export const convex = new ConvexReactClient(convexUrl);
