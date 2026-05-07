0571ddf72403bb7320500d0aa0ee1d7c077e368c
wahyu ikbal maulana <wahyuikbal777@gmail.com>
2026-05-07 10:00:47 +0700

    Update convex-client.ts

--- apps/web/src/lib/convex-client.ts
+++ apps/web/src/lib/convex-client.ts
@@ -1,6 +1,7 @@
 import { ConvexReactClient } from "convex/react";
 
-const convexUrl = process.env.NEXT_PUBLIC_CONVEX_URL;
+// const convexUrl = process.env.NEXT_PUBLIC_CONVEX_URL;
+const convexUrl = process.env.NEXT_PUBLIC_CONVEX_URL || "https://dev-convex-url.convex.cloud";
 
 if (!convexUrl) {
 	throw new Error("NEXT_PUBLIC_CONVEX_URL is not set bjir");
